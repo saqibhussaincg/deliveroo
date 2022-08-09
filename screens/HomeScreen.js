cbb82b81fb282b274eae2b76bd2771b811c5903c
Saqib Hussain - Developer - UI / UX Designer <saqibhussaincg@gmail.com>
2022-08-09 12:22:37 -0700

    conti

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,6 +1,15 @@
 import { View, Text, SafeAreaView, Image } from 'react-native'
 import React, { useLayoutEffect } from 'react'
 import { useNavigation } from '@react-navigation/native';
+
+import{
+    ChevronDownIcon,
+    UserIcon,
+    SearchIcon,
+    AdjustmentIcon,
+} from 'react-native-heroicons/outline';
+
+
 const HomeScreen = () => {
     const navigation = useNavigation();
 
@@ -16,15 +25,24 @@ const HomeScreen = () => {
 
   return (
     <SafeAreaView>
-      <Text className='text-red-500'>
+      <Text>
         {/* Header */}
-        <View>
+        <View className="flex-row pb-3 item-center mx-4 space-x-2">
             <Image
             source={{
                 url: 'https://links.papareact.com/wru'
             }}
             className="h-7 w-7 bg-gray-300 p-4 rounded-full"
              />
+
+        <View>
+            <Text className='font-bold text-gray-400 text-xs'>Deliver Now!</Text>
+            <Text className='font-bold text-xl'>Current Location
+            <ChevronDownIcon size={20} color="00CCBB" />
+            </Text>
+        </View>
+
+
         </View>
       </Text>
     </SafeAreaView>
